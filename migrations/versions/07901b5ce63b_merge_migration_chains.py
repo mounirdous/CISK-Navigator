48e3f688a27528a8eaee473384eddc689d900108
Mounir Dous <mounir.dous@gmail.com>
2026-03-10 18:20:01 +0100

    Add merge migration to fix branched migration chains

---
+++ migrations/versions/07901b5ce63b_merge_migration_chains.py
@@ -0,0 +1,21 @@
+"""merge migration chains
+
+Revision ID: 07901b5ce63b
+Revises: b2c3d4e5f6a7, c7d9e5f8a2b1
+Create Date: 2026-03-10 18:19:17.892729
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "07901b5ce63b"
+down_revision = ("b2c3d4e5f6a7", "c7d9e5f8a2b1")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
