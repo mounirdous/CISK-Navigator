908dd12c805cf05ade5a47fa5d739301cfb113a9
Mounir Dous <mounir.dous@gmail.com>
2026-03-16 21:22:19 +0100

    Bump version to 2.1.4
    Update version number and changelog to reflect hotfix changes

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask import Flask
 from app.config import config
 from app.extensions import db, login_manager, migrate
 
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 
 # Enable INFO level logging for aggregation service
 logging.basicConfig(level=logging.INFO)
